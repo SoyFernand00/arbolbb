be089d93594d5ffd010d3bb4850dfdae00567d19
SoyFernand00 <n00443358@upn.pe>
2026-05-12 12:15:46 -0500

    Agregar archivos de proyecto.

---
+++ Arbolbb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbolbb
+{
+    internal class Arbolbb
+    {
+
+        Nodo raiz;
+
+        public Arbolbb()
+        {
+            raiz = null;
+        }
+
+    }
+}
---
+++ Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbolbb
+{
+    internal class Nodo
+    {
+        int dato;
+        Nodo izquierda;
+        Nodo derecha;
+
+        public Nodo(int dato)
+        {
+            this.dato = dato;
+            izquierda = null;
+            derecha = null;
+        }
+    }
+}
